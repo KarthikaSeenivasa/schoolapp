ca44307ee7cb0aa87f666ec4c20cfa57f0e70b63
rengn.96 <rengn.96@gmail.com>
2018-09-19 23:12:03 +0530

    Removed improper line endings

--- src/components/app/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Dashboard = (props) => {
-    return (<h1>Dashboard component is getting ready</h1>)
-}
-
-export default Dashboard;
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import User from './User';
-
-export default combineReducers({
-  user: User
-});
